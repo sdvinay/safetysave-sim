277dea56bff6589bce18dd85bbd7dfdf360a178a
Vinay Kumar <sdvinay@gmail.com>
2015-11-23 22:34:00 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+This is a project to simulate TTO's persistence mechanisms.
+
+The idea is to simulate each moving part in the chain independently,
+and configure each, and then look at the impact on each part.  For
+example, as we change the TTO taxapp safety-save frequency, what are
+the impacts on downstream components.
+
+Eventually, it will be possible to change the configurations in real
+time, and see the impact of config changes (spikes, waves, etc).
+
+These are the components to model
+* Phase 1 (TTO side in detail, CFP as monolothic sink)
+** User
+** Taxapp
+** Tomcat cache (including the cache crawler)
+** Return Logic stub (e.g., don't do anything but count the metrics)
+* Phase 2 (CFP side in detail)
+** Return Logic impl
+** SP-Caching
+** Couchbase
+** Couchbase crawler
+** DP
---
+++ src/taxapp.js
@@ -0,0 +1,41 @@
+// Models the TTO taxapp's persistence logic.
+
+// Public methods (called by user)
+// start() // starts a session.  Does an initial write(?)
+// touch() // marks the last accessed time, possibly triggers safety save
+// exit()   // does a full save
+
+// Internal logic
+
+// Do safety save after SAFETY_SAVE_INTERVAL seconds have passed()
+//   Triggered by touch()
+// Do full save on exit()
+// Expire after INACTIVITY_TIMEOUT seconds have passed
+//   Triggered by recurring idle thread (IDLE_THREAD_INTERVAL)
+
+// Internal state
+//   authID
+//   lastSaveTime
+//   lastAccessTime
+
+// Configuration
+// SAFETY_SAVE_INTERVAL
+// INACTIVITY_TIMEOUT
+// IDLE_THREAD_INTERVAL
+
+var Taxapp = function(authID) {
+	this.authID = authID;
+};
+
+Taxapp.prototype.start = function() {
+	// TODO
+};
+
+Taxapp.prototype.touch = function() {
+	// TODO
+};
+
+Taxapp.prototype.exit = function() {
+	// TODO
+};
+
